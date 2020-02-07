3d89efad4506f5893adf46d70643a1005d0ab4f7
Zach Davis <zach@castironcoding.com>
2020-02-07 13:01:31 -0800

    [WIP]

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const labels = core.getInput("labels");
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  // console.log(payload, 'payload');
+  const match = true;
+  //
+  // const match = payload.issue.labels
+  //
+  core.setOutput("match", match);
+  console.log(match, "Match");
 } catch (error) {
   core.setFailed(error.message);
 }
