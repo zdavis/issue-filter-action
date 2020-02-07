776ba49a23f05e2a0715e4fa048da2dadb8757f6
Zach Davis <zach@castironcoding.com>
2020-02-07 13:11:31 -0800

    WIP

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const github = require('@actions/github');
 
 try {
   const labels = core.getInput("labels");
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const debug = core.getInput("debug");
+  const issue = github.context.payload.issue;
+  const action = github.context.payload.acction;
 
-  // console.log(payload, 'payload');
-  const match = true;
-  //
-  // const match = payload.issue.labels
-  //
-  core.setOutput("match", match);
-  console.log(match, "Match");
+  function finish(match) {
+    const msg = match ? "Issue matches" : "Issue does not match";
+    console.log(msg);
+    core.setOutput("match", match);
+  }
+
+  if (debug) {
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  }
+
+  if (!issue) return finish(false);
+  if (!Array.isArray(issue.labels)) return finish(false);
+  return finish(issues.labels.some((label) => labels.includes(label.name)));
+  
 } catch (error) {
   core.setFailed(error.message);
 }
